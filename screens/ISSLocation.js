ce6ab8af5ba15e15722b3ac6604ab889d1c10ea6
Dhriti Dhall <74966124+DhRiTiD@users.noreply.github.com>
2022-04-05 17:11:03 +0530

    Add files via upload

---
+++ screens/ISSLocation.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default class ISSLocationScreen extends React.Component {
+  render(){
+    return(
+      <View style={styles.container}>
+      <Text>
+      ISS Realtime Location
+      </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex : 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+
+
+})
